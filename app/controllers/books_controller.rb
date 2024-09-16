016ca71086e5f9a9b4068186c5704eb442702a85
EC2 Default User <175428025+tomita0731@users.noreply.github.com>
2024-09-16 13:31:25 +0000

    [Add]新規投稿,index,show,editページ,update,destroy機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,41 @@
 class BooksController < ApplicationController
+  
+  
+  def create
+    book = Book.new(list_params)
+    book.save
+    redirect_to book_path(book)
+  end
+  
   def show
+    @book = Book.find(params[:id])
   end
 
   def index
+    @books = Book.all
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(list_params)
+    redirect_to book_path(book)
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy(list_params)
+    redirect_to books_path
+  end
+  
+
+
+private
+  def list_params
+    params.require(:book).permit(:title, :body)
+  end
+  
 end
