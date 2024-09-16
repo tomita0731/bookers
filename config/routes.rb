1fb27a8c465f1a49b9794232239e0adb029ea19d
EC2 Default User <175428025+tomita0731@users.noreply.github.com>
2024-09-16 04:09:17 +0000

    controller_books show index edit 再作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'books/show'
+  get 'books/index'
+  get 'books/edit'
   get 'homes/top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
